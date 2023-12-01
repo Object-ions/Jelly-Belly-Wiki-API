0c521cc17086278f16017ecc0aba112f0a6a961c
object-ions <moshikoatia@gmail.com>
2023-12-01 00:21:35 -0800

    Update to use MySql

--- JellyBellyWikiApi.Solution/Program.cs
+++ JellyBellyWikiApi.Solution/Program.cs
@@ -1,15 +1,24 @@
+using JellyBellyWikiApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<JellyBellyWikiApiContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
