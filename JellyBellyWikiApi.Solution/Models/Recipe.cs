1f5cf4bcfd640cae771a38524e0690edc55d3fd1
object-ions <moshikoatia@gmail.com>
2023-12-01 20:16:38 -0800

    Add class

---
+++ JellyBellyWikiApi.Solution/Models/Recipe.cs
@@ -0,0 +1,9 @@
+namespace JellyBellyWikiApi.Models
+{
+  public class Recipe
+  {
+    public int RecipeId { get; set; }
+    public string Name { get; set; }
+    public string Combination { get; set; }
+  }
+}
