8b034cba20db15400dae0a18dabf39e65668555c
object-ions <moshikoatia@gmail.com>
2023-12-01 19:34:28 -0800

    Update migration

---
+++ JellyBellyWikiApi.Solution/Migrations/20231202032834_SeedMockWithFacts.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JellyBellyWikiApi.Migrations
+{
+    public partial class SeedMockWithFacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Mixes",
+                table: "Beans");
+
+            migrationBuilder.CreateTable(
+                name: "Facts",
+                columns: table => new
+                {
+                    FactId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Facts", x => x.FactId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.InsertData(
+                table: "Facts",
+                columns: new[] { "FactId", "Description", "Title" },
+                values: new object[,]
+                {
+                    { 1, "Jelly Belly beans gained national attention when they were featured in the White House during the 1980s. President Ronald Reagan was a big fan of Jelly Belly beans, and they became known as the 'President's candy.'.", "Presidential Favorite" },
+                    { 2, "Jelly Belly is known for its extensive flavor selection, with over 100 different flavors available. Some of the more unusual flavors include Buttered Popcorn, Dr. Pepper, and even Harry Potter-themed flavors like Bertie Bott's Every Flavor Beans.", "Over 100 Flavors" },
+                    { 3, "Jelly Belly beans have been used to create intricate pieces of art. Artists and enthusiasts have used the beans to make mosaics, portraits, and sculptures.", "Bean Art" },
+                    { 4, "The Very Cherry flavor was one of the first flavors created by Jelly Belly, and it remains one of the most popular to this day.", "First Flavor" },
+                    { 5, "Making Jelly Belly beans is a complex process. It involves several stages, including creating the flavor centers, adding the candy shell, and polishing the beans to achieve their signature shine.", "Behind the Scenes" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Facts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Mixes",
+                table: "Beans",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Beans",
+                keyColumn: "BeanId",
+                keyValue: 1,
+                column: "Mixes",
+                value: "");
+        }
+    }
+}
--- JellyBellyWikiApi.Solution/Models/JellyBellyWikiApiContext.cs
+++ JellyBellyWikiApi.Solution/Models/JellyBellyWikiApiContext.cs
@@ -23,7 +23,6 @@ namespace JellyBellyWikiApi.Models
           FlavorName = "Buttered Popcorn",
           Description = "Experience the delicious taste of buttered popcorn in a bite-sized jelly bean.",
           Ingredients = "Sugar, Corn Syrup, Modified Food Starch, Contains 2% or less of the following: Natural and Artificial Flavors, Color Added, Beeswax, Carnauba Wax, Confectioner's Glaze.",
-          Mixes = "",
           ColorGroup = "White",
           BackgroundColor = "#E1DFE1",
           ImageUrl = "https://cdn-tp1.mozu.com/9046-m1/cms/files/75fea694-ed38-4e84-a86e-182e31ea5a7b",
@@ -39,14 +38,36 @@ namespace JellyBellyWikiApi.Models
         new Fact
         {
           FactId = 1,
-          Title = "Presidential Favorite"
+          Title = "Presidential Favorite",
           Description = "Jelly Belly beans gained national attention when they were featured in the White House during the 1980s. President Ronald Reagan was a big fan of Jelly Belly beans, and they became known as the 'President's candy.'."
-        }
+        },
+
         new Fact
         {
           FactId = 2,
-          Title = "Over 100 Flavors"
+          Title = "Over 100 Flavors",
           Description = "Jelly Belly is known for its extensive flavor selection, with over 100 different flavors available. Some of the more unusual flavors include Buttered Popcorn, Dr. Pepper, and even Harry Potter-themed flavors like Bertie Bott's Every Flavor Beans."
+        },
+
+        new Fact
+        {
+          FactId = 3,
+          Title = "Bean Art",
+          Description = "Jelly Belly beans have been used to create intricate pieces of art. Artists and enthusiasts have used the beans to make mosaics, portraits, and sculptures."
+        },
+
+        new Fact
+        {
+          FactId = 4,
+          Title = "First Flavor",
+          Description = "The Very Cherry flavor was one of the first flavors created by Jelly Belly, and it remains one of the most popular to this day."
+        },
+
+        new Fact
+        {
+          FactId = 5,
+          Title = "Behind the Scenes",
+          Description = "Making Jelly Belly beans is a complex process. It involves several stages, including creating the flavor centers, adding the candy shell, and polishing the beans to achieve their signature shine."
         }
       );
     }
