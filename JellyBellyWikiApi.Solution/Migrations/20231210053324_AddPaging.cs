a2a119399269790a0163f3487f072240eac6a605
object-ions <moshikoatia@gmail.com>
2023-12-09 21:57:21 -0800

    Update migration to add pagination

---
+++ JellyBellyWikiApi.Solution/Migrations/20231210053324_AddPaging.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JellyBellyWikiApi.Migrations
+{
+    public partial class AddPaging : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
