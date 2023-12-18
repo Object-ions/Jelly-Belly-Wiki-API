eed8fa4b282a287b3e353a797a91cce734db5d35
object-ions <moshikoatia@gmail.com>
2023-12-17 19:11:45 -0800

    Update migrations

---
+++ JellyBellyWikiApi.Solution/Migrations/20231218031116_AddNewModelRecipe.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JellyBellyWikiApi.Migrations
+{
+    public partial class AddNewModelRecipe : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
