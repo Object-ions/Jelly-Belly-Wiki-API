19c7a875e1d887c3e39cc40444d451afae5d492d
object-ions <moshikoatia@gmail.com>
2023-12-17 20:24:19 -0800

    Update model to take array of strings. WIP

--- JellyBellyWikiApi.Solution/Models/Recipe.cs
+++ JellyBellyWikiApi.Solution/Models/Recipe.cs
@@ -1,3 +1,6 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+
 namespace JellyBellyWikiApi.Models
 {
   public class Recipe
@@ -11,9 +14,34 @@ namespace JellyBellyWikiApi.Models
     public string MakingAmount { get; set; }
     public string ImageUrl { get; set; }
     public string[] Ingredients { get; set; }
+
+    public string Additions1Json 
+    { 
+      get => JsonSerializer.Serialize(Additions1);
+      set => Additions1 = JsonSerializer.Deserialize<string[]>(value);
+    }
+
+    [NotMapped]
     public string[] Additions1 { get; set; }
+
+    public string Additions2Json 
+    { 
+      get => JsonSerializer.Serialize(Additions2);
+      set => Additions2 = JsonSerializer.Deserialize<string[]>(value);
+    }
+
+    [NotMapped]
     public string[] Additions2 { get; set; }
+
+    public string Additions3Json 
+    { 
+      get => JsonSerializer.Serialize(Additions3);
+      set => Additions3 = JsonSerializer.Deserialize<string[]>(value);
+    }
+
+    [NotMapped]
     public string[] Additions3 { get; set; }
+
     public string[] Directions { get; set; }
     public string[] Tips { get; set; }
   }
