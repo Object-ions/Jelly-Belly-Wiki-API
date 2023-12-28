6a6e79520a71c619cd1006c3b9f1bac9f37f41ef
object-ions <moshikoatia@gmail.com>
2023-12-27 16:44:04 -0800

    Refactor Bean model to serialize and deserialize GroupName and Ingredients fields, ensuring that only array representations are included in JSON responses while serialized string versions are hidden.

--- JellyBellyWikiApi.Solution/Models/Bean.cs
+++ JellyBellyWikiApi.Solution/Models/Bean.cs
@@ -1,31 +1,49 @@
 using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace JellyBellyWikiApi.Models
 {
-  public class Bean
-  {
-    public int BeanId { get; set; }
-
-    [NotMapped]
-    public string[] GroupName { get; set; }
-
-    public string GroupNameSerialized
+    public class Bean
     {
-      get => GroupName == null ? null : string.Join(", ", GroupName);
-      set => GroupName = string.IsNullOrEmpty(value) ? Array.Empty<string>() : value.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+        public int BeanId { get; set; }
+
+        // GroupName
+        [NotMapped]
+        public string[] GroupName { get; set; }
+
+        [JsonIgnore]
+        public string GroupNameSerialized
+        {
+            get => GroupName == null ? null : string.Join(", ", GroupName);
+            set => GroupName = string.IsNullOrEmpty(value) ? Array.Empty<string>() : value.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    
+        // Ingredients
+        [JsonIgnore]
+        public string IngredientsSerialized
+        {
+            get => _ingredientsSerialized;
+            set 
+            {
+                _ingredientsSerialized = value;
+                Ingredients = string.IsNullOrEmpty(value) ? Array.Empty<string>() : value.Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+
+        [NotMapped]
+        public string[] Ingredients { get; set; }
+
+        private string _ingredientsSerialized; 
+
+        public string FlavorName { get; set; }
+        public string Description { get; set; }
+        public string ColorGroup { get; set; }
+        public string BackgroundColor { get; set; }
+        public string ImageUrl { get; set; }
+        public bool GlutenFree { get; set; }
+        public bool SugarFree { get; set; }
+        public bool Seasonal { get; set; }
+        public bool Kosher { get; set; }
     }
-    public string FlavorName { get; set; }
-    public string Description { get; set; }
-    public string ColorGroup { get; set; }
-    public string BackgroundColor { get; set; }
-    public string ImageUrl { get; set; }
-    public string[] Ingredients { get; set; }
-    public bool GlutenFree { get; set; }
-    public bool SugarFree { get; set; }
-    public bool Seasonal { get; set; }
-    public bool Kosher { get; set; }
-  }
 }
