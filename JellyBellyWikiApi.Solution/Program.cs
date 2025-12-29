57fdf18a05dc9f081a4547526cea9371cd3bd566
Moses Poston <56145080+Object-ions@users.noreply.github.com>
2025-12-29 09:59:46 -0700

    Disable config file watching in Production

--- JellyBellyWikiApi.Solution/Program.cs
+++ JellyBellyWikiApi.Solution/Program.cs
@@ -3,49 +3,49 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Disable config file watching in Production
+if (builder.Environment.IsProduction())
+{
+    builder.Configuration.Sources.Clear();
+    builder.Configuration
+        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+        .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: false)
+        .AddEnvironmentVariables();
+}
+
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<JellyBellyWikiApiContext>(
-                  dbContextOptions => dbContextOptions
-                    .UseMySql(
-                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
-                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
-                    )
-                  )
-                );
+builder.Services.AddDbContext<JellyBellyWikiApiContext>(dbContextOptions =>
+    dbContextOptions.UseMySql(
+        builder.Configuration["ConnectionStrings:DefaultConnection"],
+        ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
+    )
+);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddCors(options =>
-      {
-        options.AddDefaultPolicy(
-            policy =>
-            {
 
-              policy.AllowAnyOrigin();
-              policy.AllowAnyHeader();
-              policy.AllowAnyMethod();
-            });
-      });
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin();
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// if (app.Environment.IsDevelopment())
-// {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-// }
-// else 
-// {
-  app.UseHttpsRedirection();
-// }
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
 
 app.UseCors();
-
 app.UseAuthorization();
 
 app.MapControllers();
-
- app.MapFallbackToController("Get", "Home");
+app.MapFallbackToController("Get", "Home");
 
 app.Run();
